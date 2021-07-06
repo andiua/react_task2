4a687316817271903b32f9f66b285534fcc6d1d0
andiua <anduhust@ukr.net>
2021-07-07 00:34:28 +0300

    api settings, random character

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,32 +1,77 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import GotService from '../../services/gotService';
+import Spinner from '../spinner';
 import './randomChar.css';
 
 export default class RandomChar extends Component {
+	constructor() {
+		super();
+		this.upDateChar();
+	}
 
-    render() {
+	gotService = new GotService();
+	state = {
+		char: {},
+		loading: true,
+		error: false
+	};
 
-        return (
-            <div className="random-block rounded">
-                <h4>Random Character: John</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>11.03.1039</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>13.09.1089</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>Anarchy</span>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+	onCharLoaded = char => {
+		this.setState({
+			char,
+			loading: false
+		});
+	};
+
+	upDateChar = () => {
+		const id = Math.floor(Math.random() * 140 + 25);
+		this.gotService.getCharacter(id).then(char => {
+			this.onCharLoaded(char);
+		})
+		.catch(this.onError);
+	};
+
+	onError = (err) => {
+		this.setState({
+			error: true,
+			loading: false
+		});
+	}
+
+	render() {
+		const {
+			char: { name, gender, born, died, culture }, loading } = this.state;
+
+		if (loading) {
+			return (
+				<div className="random-block rounded">
+					<Spinner />
+				</div>
+			);
+		}
+
+		return (
+			<div className="random-block rounded">
+				<h4>Random Character: {name}</h4>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item d-flex justify-content-between">
+						<span className="term">Gender </span>
+						<span>{gender}</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between">
+						<span className="term">Born </span>
+						<span>{born}</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between">
+						<span className="term">Died </span>
+						<span>{died}</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between">
+						<span className="term">Culture </span>
+						<span>{culture}</span>
+					</li>
+				</ul>
+			</div>
+		);
+	}
 }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+	return (
+		<div className="loadingio-spinner-spin-9i2pxyiz88s">
+			<div className="ldio-jdxlr8cti5m">
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+				<div>
+					<div />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Spinner;
