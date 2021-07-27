bb5cf698f43f25941737fcafa8563482b00892f8
andiua <anduhust@ukr.net>
2021-07-28 00:51:48 +0300

    practice with hooks by rewriting few components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,68 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Row, Container, Button } from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorComponent from '../error';
 import { CharacterPage, BookPage, HousePage, BookItem } from '../pages';
-import GotService from '../../services/gotService';
 import './app.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-export default class App extends Component {
-	gotService = new GotService();
-	state = {
-		displayRandomChar: true,
-		error: false
-	};
-
-	componentDidCatch() {
-		console.log('error');
-		this.setState({
-			error: true
-		});
-	}
-
-	toggleRandomChar = () => {
-		this.setState(preState => {
-			return {
-				displayRandomChar: !preState.displayRandomChar
-			};
-		});
-	};
-
-	render() {
-		const { displayRandomChar, error } = this.state;
-		const randomChar = displayRandomChar ? <RandomChar interval={4000} /> : null;
-		if (error) {
-			return <ErrorComponent />;
+function App () {
+	const [displayRandomChar, toggleRandomChar] = useState(true)
+	const [error, onError] = useState(false)
+	useEffect(() => {
+		return () => { //Component DIDCATCH в return передавати у ФУНКЦІЮ!
+			console.log('error');
+			onError(true) // новий стан передаєтья або як аргумент, або як результат функції
 		}
+	})
 
-		return (
-			<Router>
-				<div className="app">
-					<Container>
-						<Header />
-					</Container>
-					<Container>
-						<Row>
-							<Col lg={{ size: 5, offset: 0 }}>
-								{randomChar}
-								{/* <RandomChar display={displayRandomChar} />  моє не ненайкраще виконання*/}
-								<Button onClick={this.toggleRandomChar} className="toggle-random" color="secondary">
-									Toggle random character
-								</Button>
-							</Col>
-						</Row>
-						<Route path='/characters' exact component={CharacterPage}/>
-						<Route path='/houses' exact component={HousePage}/>
-						<Route path='/books' exact component={BookPage}/>
-						<Route path='/books/:id' exact render={ ({match}) => {
-							const {id} = match.params;
-							return <BookItem bookId={id} />
-						}}/>
-					</Container>
-				</div>
-			</Router>
-		);
+	const randomChar = displayRandomChar ? <RandomChar interval={4000} /> : null;
+	if (error) {
+		return <ErrorComponent />;
 	}
+
+	return (
+		<Router>
+			<div className="app">
+				<Container>
+					<Header />
+				</Container>
+				<Container>
+					<Row>
+						<Col lg={{ size: 5, offset: 0 }}>
+							{randomChar}
+							{/* <RandomChar display={displayRandomChar} />  моє не ненайкраще виконання*/}
+							<Button onClick={() => toggleRandomChar(!displayRandomChar)} className="toggle-random" color="secondary">
+								Toggle random character
+							</Button>
+						</Col>
+					</Row>
+					<Route path='/characters' exact component={CharacterPage}/>
+					<Route path='/houses' exact component={HousePage}/>
+					<Route path='/books' exact component={BookPage}/>
+					<Route path='/books/:id' exact render={ ({match}) => {
+						const {id} = match.params;
+						return <BookItem bookId={id} />
+					}}/>
+				</Container>
+			</div>
+		</Router>
+	);
 }
+export default App;
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,108 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import GotService from '../../services/gotService';
 import Spinner from '../spinner';
 import ErrorComponent from '../error';
 import './randomChar.css';
 
-export default class RandomChar extends Component {
+function RandomChar({interval = 1000} ) {
 
-	gotService = new GotService();
-	state = {
-		char: {},
-		loading: true,
-		error: false
-	};
-	static defaultProps = { //щоб задати дефолтні пропси. прописуємо в компоненті властивість defaultProps. це нова можливість es9
-		interval: 1000
+	const gotService = new GotService();
+	const [char, onCharLoaded] = useState({});
+	const [loading, onLoaded] = useState(true);
+	const [error, onError] = useState(false);
+
+	if (typeof interval !== 'number' && isNaN(interval)){
+		return new TypeError(`RandomChar: ${interval} must be a number`);
 	}
-	static propTypes = {
-		interval: (props, propName, componentName) => {
-			const value = props[propName];
 
-			if (typeof value === 'number' && !isNaN(value)){
-				return null;
-			}
-			return new TypeError(`${componentName}: ${propName} must be a number`);
+	useEffect( () => {
+		upDateChar();
+		let timerId = setInterval(upDateChar, interval);
+		onLoaded(false);
+		return () => {
+			clearTimeout(timerId);
 		}
-	}
-
-	componentDidMount() { //викликається як компонент закічив рендитиритись після 1 ініціалізації
-		this.upDateChar();
-		this.timerId = setInterval(this.upDateChar, this.props.interval);
-	}
-
-	componentWillUnmount() {//викликається перед видаленням компонента
-		clearTimeout(this.timerId);
-	}
-
-	onCharLoaded = char => {
-		this.setState({
-			char,
-			loading: false
-		});
-	};
-
-	upDateChar = () => {
+	}, [interval]) 
+	function upDateChar() {
 		const id = Math.floor(Math.random() * 140 + 25);
-		this.gotService.getCharacter(id).then(char => {
-			this.onCharLoaded(char);
+		gotService.getCharacter(id).then(newChar => {
+			onCharLoaded(newChar);
 		})
-		.catch(this.onError);
+		.catch(() => {
+			onError(true);
+		});
 	};
 
-	onError = (err) => {
-		this.setState({
-			error: true,
-			loading: false
-		});
-	}
+	const { name, gender, born, died, culture } = char;
 
-	render() {
-		const {
-			char: { name, gender, born, died, culture }, loading, error } = this.state;
-
-		if (loading) {
-			return (
-				<div className="random-block rounded">
-					<Spinner />
-				</div>
-			);
-		}
-
-		if(error) {
-			return (
-				<div className="random-block rounded">
-					<ErrorComponent />
-				</div>
-			)
-		}
-
+	if (loading) {
 		return (
 			<div className="random-block rounded">
-				<h4>Random Character: {name}</h4>
-				<ul className="list-group list-group-flush">
-					<li className="list-group-item d-flex justify-content-between">
-						<span className="term">Gender </span>
-						<span>{gender}</span>
-					</li>
-					<li className="list-group-item d-flex justify-content-between">
-						<span className="term">Born </span>
-						<span>{born}</span>
-					</li>
-					<li className="list-group-item d-flex justify-content-between">
-						<span className="term">Died </span>
-						<span>{died}</span>
-					</li>
-					<li className="list-group-item d-flex justify-content-between">
-						<span className="term">Culture </span>
-						<span>{culture}</span>
-					</li>
-				</ul>
+				<Spinner />
 			</div>
 		);
 	}
-}
 
+	if(error) {
+		return (
+			<div className="random-block rounded">
+				<ErrorComponent />
+			</div>
+		)
+	}
+
+	return (
+		<div className="random-block rounded">
+			<h4>Random Character: {name}</h4>
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item d-flex justify-content-between">
+					<span className="term">Gender </span>
+					<span>{gender}</span>
+				</li>
+				<li className="list-group-item d-flex justify-content-between">
+					<span className="term">Born </span>
+					<span>{born}</span>
+				</li>
+				<li className="list-group-item d-flex justify-content-between">
+					<span className="term">Died </span>
+					<span>{died}</span>
+				</li>
+				<li className="list-group-item d-flex justify-content-between">
+					<span className="term">Culture </span>
+					<span>{culture}</span>
+				</li>
+			</ul>
+		</div>
+	);
+}
+export default RandomChar;
 // RandomChar.defaultProps = { //щоб задати дефолтні пропси. прописуємо в компоненті властивість defaultProps
 // 	interval: 1000
 // }
